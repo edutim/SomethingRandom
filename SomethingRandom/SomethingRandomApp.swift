39b911d5de64887796464a4564d7fe5ab8cb069e
Tim Hart <thart@apple.com>
2023-03-09 09:08:42 -0500

    Initial Commit

---
+++ SomethingRandom/SomethingRandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingRandomApp.swift
+//  SomethingRandom
+//
+//  Created by Timothy Hart on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SomethingRandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
